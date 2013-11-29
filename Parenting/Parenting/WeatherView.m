8bc757007d641ae25d4b0a08b122c758712faade
Jamming <arvilinam@gmail.com>
2013-11-29 16:29:01 +0800

    v1.4.0 11291628

--- Parenting/Parenting/WeatherView.m
+++ Parenting/Parenting/WeatherView.m
@@ -281,7 +281,6 @@
     }
     DXAlertView *alert = [[DXAlertView alloc] initWithTitle:title contentText:mAd.mContent leftButtonTitle:@"Ok" rightButtonTitle:nil];
     [alert show];
-    //[self pushViewController:wavc animated:YES];
 }
 
 @end
